2584dd04a6e4dfa3a9cfae94a2f7fc4ee6b1f39c
Bryn <bryn@Bryns-MacBook-Pro.local>
2018-10-01 00:39:36 -0500

    App build and basic architecture init

---
+++ app/components/main/mainCtrl.js
@@ -0,0 +1,25 @@
+(function(app) {
+    function MainCtrl () {
+        var vm = this;
+
+        vm.menu = [{
+            name: "State",
+            component: "state"
+        }, {
+            name: "County",
+            component: "county"
+        }, {
+            name: "City",
+            component: "city"
+        }];
+    }
+    
+    app.component('main', {
+        // Loading the template
+        templateUrl: '/app/components/main/main.html',
+        controller: MainCtrl
+    });
+    
+})(app);
+
+
---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    browserify = require('browserify'),
+    transform = require('vinyl-transform'),
+    source = require('vinyl-source-stream');
+
+var appJsSrc = ['app/**/*.js'],
+    stylesSrc = 'assets/styles/build.scss',
+    outputDir = 'build';
+
+gulp.task('buildJs', function() {
+    gulp.src(appJsSrc)
+        .pipe(concat('app.js'))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('build'))
+});
+
+gulp.task('buildCss', function() {
+    gulp.src(stylesSrc)
+        .pipe(sass({style: 'expanded'}))
+        .pipe(concat('app.css'))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('build'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch(appJsSrc, ['buildJs']);
+  gulp.watch(stylesSrc, ['buildCss']);
+});
+
+gulp.task('default', ['buildJs', 'buildCss', 'watch']);
